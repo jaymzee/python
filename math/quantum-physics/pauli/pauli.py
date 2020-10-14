ff0de569778289710e8aea7e26f55c5b610aeb8d
jaymzee <jaymzee@users.noreply.github.com>
2020-10-14 14:59:10 -0400

    pauli - list comprehensions and enumerate
    more pythonic!

--- math/quantum-physics/pauli/pauli.py
+++ math/quantum-physics/pauli/pauli.py
@@ -4,15 +4,14 @@ import numpy as np
     np.array([[0, -1j],[1j, 0]]),
     np.array([[1, 0],[0, -1]])]
 
-x = []
-for i in range(3):
-    x.append(np.linalg.eig(σ[i])[1])
+x = [np.linalg.eig(m)[1] for m in σ]
 
 print('Pauli Matrices:')
-for i in range(3):
-    print("σ%d =\n%s\n" % (i + 1, σ[i]))
+for i, m in enumerate(σ, 1):
+    print("σ%d =\n%s\n" % (i, m))
 
 print('eigenvalues:\nλ = [1, -1]\n')
+
 print('eigenvectors:')
-for i in range(3):
-    print('x%d =\n%s\n' % (i + 1, x[i]))
+for i, v in enumerate(x, 1):
+    print('x%d =\n%s\n' % (i, v))
