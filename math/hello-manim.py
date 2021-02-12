1363828c986fabb6437d653f3f8694c4a7ad8229
jaymzee <jaymzee@users.noreply.github.com>
2021-02-12 07:02:00 -0500

    3D scene

--- math/hello-manim.py
+++ math/hello-manim.py
@@ -18,6 +18,30 @@ class TestScene(Scene):
         self.play(Write(tex))
         self.wait()
 
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(BLUE, opacity=0.5)
+        circle.set_stroke(BLUE_E, width=4)
+        square = Square()
+
+        self.play(ShowCreation(square))
+        self.wait()
+        self.play(ReplacementTransform(square, circle))
+        self.wait()
+
+
+class SurfaceExample(ThreeDScene):
+    def construct(self):
+        axes = ThreeDAxes()
+        self.move_camera(phi=70 * DEGREES, theta=-30 * DEGREES)
+        sphere = Sphere(radius=3)
+
+        self.play(ShowCreation(sphere), ShowCreation(axes))
+        self.wait()
+        
+
 class Graph2dFunc(GraphScene):
     CONFIG = {
         "x_min": -3,
@@ -36,8 +60,15 @@ class Graph2dFunc(GraphScene):
         graph2 = self.get_graph(lambda x: x**3, WHITE)
         graph_label2 = self.get_graph_label(graph2, label='x^{3}')
 
+        graph3 = self.get_graph(lambda x: max(x, 0), WHITE)
+        graph_label3 = self.get_graph_label(graph3, label='max(x, 0)')
+
         # Display graph
         self.play(ShowCreation(graph), Write(graph_label))
         self.wait(1)
-        self.play(Transform(graph, graph2), Transform(graph_label, graph_label2))
+        self.play(ReplacementTransform(graph, graph2), 
+                  ReplacementTransform(graph_label, graph_label2))
+        self.wait(1)
+        self.play(ReplacementTransform(graph2, graph3), 
+                  ReplacementTransform(graph_label2, graph_label3))
         self.wait(1)
