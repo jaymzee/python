d4c505bca5197320b2418b130f019e15c3311dc9
jaymzee <jaymzee@users.noreply.github.com>
2021-03-04 12:08:40 -0500

    animation

---
+++ math/animation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import scipy.stats
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+fig, ax = plt.subplots()
+
+x = np.linspace(0, 2 * np.pi, 100)
+line, = ax.plot(x, np.sin(x))
+
+
+def animate(i):
+    line.set_ydata(np.sin(x + i/ 50))
+    return line,
+
+ani = animation.FuncAnimation(
+        fig, animate, interval=20, blit=True, save_count=50)
+
+# to save animation, use e.g.
+# ani.save("movie.mp4")
+#
+# or
+#
+# writer = animation.FFMpegWriter(
+#   fps=15, metadata=dict(artist='Me'), bitrate=1800)
+# ani.save("movie.mp4", writer=writer)
+
+plt.show()
