c251c143617b646c43140ee8e2a639bfe01ff85b
jaymzee <jaymzee@users.noreply.github.com>
2021-03-07 09:41:24 -0500

    make gradient descent more like definition

--- math/ml/grad_desc.py
+++ math/ml/grad_desc.py
@@ -5,29 +5,27 @@ batch gradient descent
 import numpy as np
 
 def batch_gradient_descent(h, θ, X, y, **kwargs):
-    count = 0
     α = kwargs.get("alpha", 0.01)
     threshold = kwargs.get("threshold", 0.001)
-    while True:
-        count += 1
-        d = α * (h(θ, X) - y) @ X
-        θ -= d
-        if np.linalg.norm(d) < threshold:
-            break
-    return count
+    n, d = 0, 1e8
+    while np.linalg.norm(d) > threshold:
+        d = α * (y - h(θ, X)) @ X
+        n += 1
+        θ += d  # modifies θ like a reference parameter
+    return n
 
 
 # generate data set
 m = 10000               # number of samples
 n = 2                   # number of features
-X = np.vstack([np.ones(m), np.random.rand(n, m) * 1]).T
-v = np.array([3.0, 1.5, 2.1])   #  y = 3.0 + 1.5 x1 + 2.1 x2
-y = X @ v + np.random.normal(size=m, scale=0.1)
+X = np.vstack([np.ones(m), np.random.rand(n, m) * 1.0]).T
+c = np.array([3.0, 1.5, 2.1])   #  y = 3.0 + 1.5 x1 + 2.1 x2
+y = X @ c + np.random.normal(size=m, scale=0.1)
 
 # determine parameters using data set and batch gradient descent
 theta = np.zeros(n + 1) # initialize parameters
 iterations = batch_gradient_descent(
-    lambda θ, x: x @ θ,
+    lambda θ, x: np.inner(θ,x),
     theta, X, y,
     alpha=1/m,
     threshold=1E-9
--- math/ml/stoch_gd.py
+++ math/ml/stoch_gd.py
@@ -7,29 +7,29 @@ import numpy as np
 def stochastic_gradient_descent(h, θ, X, y, **kwargs):
     α = kwargs.get('alpha', 0.01)
     threshold = kwargs.get('threshold', 0.0001)
-    iterations = 0
+    n = 0
     while True:
         θ_ = θ.copy()
         for i in range(len(X)):
-            iterations += 1
+            n += 1
             xi = X[i,:]
-            θ -= α * (h(θ,xi) - y[i]) * xi
+            θ += α * (y[i] - h(θ,xi)) * xi
         if np.linalg.norm(θ - θ_) < threshold:
             break
-    return iterations
+    return n
 
 
 # generate data set
 m = 10000           # number of samples
 n = 5               # number of features
-X = np.vstack([np.ones(m), np.random.rand(n, m) * 1]).T
-v = np.array([3.0, 1.5, 2.1, 3.14, 2.712, 1.0])
-y = X @ v + np.random.normal(size=m, scale=0.1)
+X = np.vstack([np.ones(m), np.random.rand(n, m) * 1.0]).T
+c = np.array([3.0, 1.5, 2.1, 3.14, 2.712, 1.0])
+y = X @ c + np.random.normal(size=m, scale=0.1)
 
 # compute parameters from data set using stochastic gradient descent
 theta = np.zeros(n + 1) # parameters
 iterations = stochastic_gradient_descent(
-    lambda θ, x: x @ θ,
+    lambda θ, x: θ @ x,
     theta, X, y,
     alpha=0.01,
     threshold=1E-9
