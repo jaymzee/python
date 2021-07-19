3f1e0c7642e5b7481a71eabab29b966c5c39993b
jaymzee <jaymzee@users.noreply.github.com>
2021-07-18 22:02:59 -0400

    manim example

--- math/manim/ce/LaTeX/main.py
+++ math/manim/ce/LaTeX/main.py
@@ -2,10 +2,16 @@ from manim import *
 
 class ExampleText(Scene):
     def construct(self):
-        tex = Text(r'\LaTeX').scale(3)
-        self.add(tex)
+        text = Text(r'\LaTeX').scale(3)
+        self.add(text)
 
 class ExampleLaTeX(Scene):
     def construct(self):
         tex = Tex(r'\LaTeX').scale(3)
         self.add(tex)
+
+class Math(Scene):
+    def construct(self):
+        tex = MathTex(r'\int \frac{1}{x} \, dx = \log x').scale(3)
+        self.play(Write(tex))
+        self.wait()
