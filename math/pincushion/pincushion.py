1722a117fcfdd72f4454cb208e1eda20a7e542e5
jaymzee <jaymzee@users.noreply.github.com>
2021-08-03 13:57:06 -0400

    pincushion distortion

--- math/pincushion/pincushion.py
+++ math/pincushion/pincushion.py
@@ -1,21 +1,39 @@
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
-import itertools
+from matplotlib.widgets import Slider
 
-xx = np.linspace(0, 1, 10)
-yy = np.linspace(0, 1, 10)
+axcolor = 'lightgoldenrodyellow'
 
-xx -= 0.5
-yy -= 0.5
+def f(n, v):
+    x = np.linspace(-1, 1, n)
+    y = np.linspace(-1, 1, n)
+    points = itertools.product(x, y)
+    xy = np.array(list(points)).T
+    r = xy[0] ** 2 + xy[1] ** 2
+    xy /= (v + r)
+    xy *= v
+    return xy
 
-points = list(itertools.product(xx, yy))
-xy = np.array(points).T
 
-#plt.scatter(xy[0], xy[1])
-#plt.show()
+def main():
+    def update(val):
+        sc.set_offsets(f(10, val).T)
 
-r = xy[0] ** 2 + xy[1] ** 2
-xy /= (4 + r)
+    distortion_init = 4
+    xy = f(10, distortion_init)
+    sc = plt.scatter(xy[0], xy[1])
+    plt.title('pincushion distortion')
+    plt.subplots_adjust(bottom=0.25)
 
-plt.scatter(xy[0], xy[1])
-plt.show()
+    distortion = Slider(
+        ax=plt.axes([0.25, 0.1, 0.65, 0.05], facecolor=axcolor),
+        label='distortion',
+        valmin=2.0,
+        valmax=30,
+        valinit=distortion_init,
+    )
+    distortion.on_changed(update)
+    plt.show()
+
+main()
