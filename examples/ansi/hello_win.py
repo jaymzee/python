775b32bc03d43820ca644fae98371e96cf6f603e
jaymzee <jaymzee@users.noreply.github.com>
2021-08-03 09:01:19 -0400

    ansi

--- examples/ansi/hello_win.py
+++ examples/ansi/hello_win.py
@@ -1,6 +1,8 @@
 import sys
 import colorama
 
+# Windows 10 CMD prompt does not have ANSI enabled by default
+# this is easier than manually enabling the VIRTUAL_TERMINAL_PROCESSING flag
 colorama.init()
 
 sys.stdout.write('\033[1;31mHello\033[m\n')
