1ab8a962f05886f4ee2743e9657425a57690b9be
jaymzee <jaymzee@users.noreply.github.com>
2021-08-05 08:26:35 -0400

    math/primefactor

---
+++ math/primefactor.py
@@ -0,0 +1,22 @@
+import sys
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return n, factors
+
+
+if len(sys.argv) > 1:
+    n = int(sys.argv[1])
+    print(*prime_factors(n))
+else:
+    sys.stderr.write("Usage: prime-factors INTEGER\n")
+    sys.exit(2)
