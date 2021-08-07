ccdece0f0788d8fcee10b5a74d0b14529b57a070
jaymzee <jaymzee@users.noreply.github.com>
2021-08-07 11:55:38 -0400

    print 256 colors using ansi codes

---
+++ examples/ansi/256color.py
@@ -0,0 +1,16 @@
+import sys
+
+def print_color(val):
+    sys.stdout.write(f'\033[48;5;{val}m ')
+
+def print_colors(vals):
+    for i, val in enumerate(vals):
+        print_color(val)
+        if i == 0:
+            first = val
+    sys.stdout.write(f'\033[m {first}..{val}\n')
+
+print_colors(range(16))
+for i in range(16, 232, 36):
+    print_colors(i + j for j in range(36))
+print_colors(range(232,  256))
