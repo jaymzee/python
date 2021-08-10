8798c0a94fec912a0432b3aacc9f243e3ed19883
jaymzee <jaymzee@users.noreply.github.com>
2021-08-10 02:59:28 -0400

    winpipe

--- examples/pipes/winpipe.py
+++ examples/pipes/winpipe.py
@@ -1,6 +1,13 @@
-with open(r"\\.\pipe\lithium-COM1") as f:
-    while True:
-        b = f.read(1)
-        if len(b) == 0:
-            break
-        print(b)
+import os
+import time
+
+com1 = open(r"\\.\pipe\lithium-COM1", "r")
+com1fno = com1.fileno()
+
+while True:
+    #r, w, e = select([com1], [], [], 0)
+    b = os.read(com1fno, 1)
+    if len(b) == 0:
+        break
+    print(b)
+    time.sleep(1)
