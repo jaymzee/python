eae1a78865d1c5cb6b169039f99a686a6ca1b537
jaymzee <jaymzee@users.noreply.github.com>
2021-08-16 07:24:57 -0400

    winpipe - windows pipe echo server

--- examples/pipes/winpipe.py
+++ examples/pipes/winpipe.py
@@ -1,10 +1,26 @@
 import time
+import sys
 
-com1 = open(r"\\.\pipe\lithium-COM1", "r")
 
-while True:
-    b = com1.read(1)
-    if len(b) == 0:
-        break
-    print("got", b)
-    time.sleep(1)
+def echo(pipe):
+    while True:
+        rxb = pipe.read(1)
+        if len(rxb) == 0 or rxb == b'~':
+            break
+        c = rxb[0]
+        ch = chr(c) if c > 32 and c < 127 else ' '
+        print('rx:', hex(c), ch)
+        time.sleep(0.001)
+        #print('tx:', hex(c), ch)
+        pipe.write(rxb)
+
+
+if len(sys.argv) > 1:
+    filename = fr"\\.\pipe\{sys.argv[1]}"
+    with open(filename, "wb+") as pipe:
+        print(f'echoing {filename}')
+        print('send a ~ to quit')
+        echo(pipe)
+else:
+    print("Usage: winpipe pipename")
+    exit(2)
