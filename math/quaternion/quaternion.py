f69e88c6e7132cecda52df73addd39db68668f32
jaymzee <jaymzee@users.noreply.github.com>
2021-09-22 06:45:28 -0400

    math/quaternion

--- math/quaternion/quaternion.py
+++ math/quaternion/quaternion.py
@@ -1,3 +1,10 @@
+"""
+quaternions
+
+q = w + x i + y j + z k
+
+"""
+
 import numpy as np
 
 def qmult(q1, q0):
@@ -9,16 +16,26 @@ def qmult(q1, q0):
                      -x1*z0 + y1*w0 + z1*x0 + w1*y0,
                       x1*y0 - y1*x0 + z1*w0 + w1*z0])
 
+def qconj(q):
+    return np.array([q[0], -q[1], -q[2], -q[3]])
+
+def qnorm(q):
+    w, x, y, z = q
+    return w*w + x*x + y*y + z*z
+
 
 if __name__ == "__main__":
-    print(qmult([2,0,0,0], [3,0,0,0]), '2 * 3 =  6')
-    print(qmult([0,1,0,0], [0,1,0,0]), 'i * i = -1')
-    print(qmult([0,0,1,0], [0,0,1,0]), 'j * j = -1')
-    print(qmult([0,0,0,1], [0,0,0,1]), 'k * k = -1')
-    print(qmult(qmult([0,1,0,0], [0,0,1,0]), [0,0,0,1]), 'i * j * k = -1')
-    print(qmult([0,1,0,0], [0,0,1,0]), 'i * j =  k')
-    print(qmult([0,0,1,0], [0,0,0,1]), 'j * k =  i')
-    print(qmult([0,0,0,1], [0,1,0,0]), 'k * i =  j')
-    print(qmult([0,0,1,0], [0,1,0,0]), 'j * k = -k')
-    print(qmult([0,0,0,1], [0,0,1,0]), 'k * j = -i')
-    print(qmult([0,1,0,0], [0,0,0,1]), 'i * k = -j')
+    print(qmult([2,0,0,0], [3,0,0,0]), '2 · 3 = 6')
+    print(qmult([0,1,0,0], [0,1,0,0]), 'i² = -1')
+    print(qmult([0,0,1,0], [0,0,1,0]), 'j² = -1')
+    print(qmult([0,0,0,1], [0,0,0,1]), 'k² = -1')
+    print(qmult(qmult([0,1,0,0], [0,0,1,0]), [0,0,0,1]), 'i j k = -1')
+    print(qmult([0,1,0,0], [0,0,1,0]), 'i j = k')
+    print(qmult([0,0,1,0], [0,0,0,1]), 'j k = i')
+    print(qmult([0,0,0,1], [0,1,0,0]), 'k i = j')
+    print(qmult([0,0,1,0], [0,1,0,0]), 'j i = -k')
+    print(qmult([0,0,0,1], [0,0,1,0]), 'k j = -i')
+    print(qmult([0,1,0,0], [0,0,0,1]), 'i k = -j')
+
+    print('conj([1 2 3 4]) =', qconj([1,2,3,4]))
+    print('norm([1 2 3 4]) =', qnorm([1,2,3,4]))
